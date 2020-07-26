5532c13859ece0ab80e52f13fc3cf28b9727aab6
Abdullah Ansari <aa9272137@gmail.com>
2020-07-27 02:08:23 +0500

    ver 1 containing 4 questions

---
+++ script.js
@@ -0,0 +1,29 @@
+const correctAns = ['B','A','A','B'];
+let form = document.querySelector('.quiz-form');
+let result = document.querySelector('.result');
+
+form.addEventListener('submit',e =>{
+    e.preventDefault();
+    let score = 0;
+    const userAns = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    //checkans
+    userAns.forEach((answer,index)=>{
+        if(answer === correctAns[index]){
+            score+=25;
+        
+        }
+    })
+    scrollTo(0,0);
+    
+    result.classList.remove('d-none');
+    let output = 0;
+    let timer = setInterval(()=>{
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        }
+        else{
+            output++
+        }
+    },10)
+})
